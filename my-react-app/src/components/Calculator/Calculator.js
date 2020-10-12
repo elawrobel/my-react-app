cf0d499478fefe5f6e9f94cad3a99c07f2b7042b
Ela Wróbel <elzbieta.wrobel@capgemini.com>
2020-10-12 09:58:48 +0200

    add calculator

--- my-react-app/src/components/Calculator/Calculator.js
+++ my-react-app/src/components/Calculator/Calculator.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+
+function Select({ value, setCurrency }){
+  return (
+    <select value={ value } onChange={ (event) => setCurrency( event.target.value ) }>
+      <option value="USD">USD</option>
+      <option value="PLN">PLN</option>
+      <option value="GBP">GBP</option>
+      <option value="EUR">EUR</option>
+    </select>
+  )
+}
+
+
+
 
 function Calculator() {
+  const [ amount, setAmount ]  =  useState(0);
+  const [currencyFrom, setCurrencyFrom] = useState('PLN');
+  const [currencyTo, setCurrencyTo] = useState('USD');
 
   return(
 
     <form>
       <div>
-        <input type="number" placeholder="Amount" onChange={(event) => console.log(event.target.value) } />
+        <input type="number" placeholder="Amount" onChange={(event) => { setAmount(event.target.value); } } />
       </div>
       <div>
         <span>From:</span>
-        <select onChange={(event) => console.log(event.target.value) }>
-          <option value="USD">USD</option>
-          <option value="PLN">PLN</option>
-          <option value="GBP">GBP</option>
-          <option value="EUR">EUR</option>
-        </select>
+        <Select value={ currencyFrom } setCurrency= {setCurrencyFrom} />
       </div>
       <div>
         <span>To:</span>
-        <select onChange={(event) => console.log(event.target.value) }>
-          <option value="USD">USD</option>
-          <option value="PLN">PLN</option>
-          <option value="GBP">GBP</option>
-          <option value="EUR">EUR</option>
-        </select>
+        <Select value={ currencyTo } setCurrency= {setCurrencyTo} />
       </div>
-      <div>Result:</div>
+        <div>Result: {amount} </div>
     </form>
 
   );
